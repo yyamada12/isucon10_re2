bb27f107b2adae793c803509a40e84663ab4bef2
yyamada12 <12yacropolisy@gmail.com>
2021-07-03 10:15:48 +0000

    add index for order by price [696]

--- isuumo/webapp/mysql/db/0_Schema.sql
+++ isuumo/webapp/mysql/db/0_Schema.sql
@@ -34,5 +34,6 @@ CREATE TABLE isuumo.chair
     features    VARCHAR(64)     NOT NULL,
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
-    stock       INTEGER         NOT NULL
+    stock       INTEGER         NOT NULL,
+    INDEX idx_price(price)
 );
